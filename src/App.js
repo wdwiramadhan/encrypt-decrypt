1fa4a70566fcdbbee8f9e0aaa288ea2d5dc88060
wdwiramadhan <wahyudwiramadhan@gmail.com>
2021-04-21 09:14:53 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Flex, Textarea, Text, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import CryptoJs from "crypto-js";
 
 function App() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const secretKey = "thisissecretkey";
+  const encrypt = () => {
+    const cipherText = CryptoJs.AES.encrypt(text, secretKey).toString();
+    setResult(cipherText);
+  };
+  const decrypt = () => {
+    const plainText = CryptoJs.AES.decrypt(text, secretKey);
+    setResult(plainText.toString(CryptoJs.enc.Utf8));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex flexDirection="row" justifyContent="center" p={4}>
+      <Box boxSize="2xl" textAlign="center">
+        <Text fontSize="3xl">Encrypt Decrypt</Text>
+        <Text fontSize="xl">Advanced Encryption Standard (AES)</Text>
+        <Textarea
+          mt={8}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Input plaintext or cipherext here"
+        />
+        <Flex justifyContent="center" mt={8}>
+          <Button colorScheme="telegram" onClick={encrypt}>
+            Encrypt
+          </Button>
+          <Button colorScheme="gray" ml="12px" onClick={decrypt}>
+            Decrypt
+          </Button>
+        </Flex>
+        <Textarea
+          mt={8}
+          value={result}
+          placeholder="Result"
+          onChange={(e) => setResult(e.target.value)}
+        />
+      </Box>
+    </Flex>
   );
 }
 
